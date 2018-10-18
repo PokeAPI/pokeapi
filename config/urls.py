1c9ad67f4cbc33d108d0044a330af8d54d182a72
Charles Marttinen <mail@charlesmarttinen.ca>
2018-10-18 14:14:54 -0400

    chore: Remove API v1

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,8 @@ from __future__ import unicode_literals
 
 from django.conf.urls import include, url
 
-from pokemon import urls as pokemon_urls
 from pokemon_v2 import urls as pokemon_v2_urls
 
 urlpatterns = [
-    url(r'^', include(pokemon_urls)),
     url(r'^', include(pokemon_v2_urls)),
 ]
--- pokemon/utils.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-import time
-
-
-def unique_filename(instance, old_filename):
-    filename = str(time.time()) + '.png'
-    return 'img/' + filename

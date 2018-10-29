4f8a2928404dbfc1e273f204bcf2d4c21e22f8a1
Charles Marttinen <mail@charlesmarttinen.ca>
2018-10-29 16:22:25 -0400

    fix: Revert db setup config.local to SQLite

--- config/local.py
+++ config/local.py
@@ -1,19 +1,19 @@
 from .settings import *
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pokeapi',
-        'USER': 'root',
-        'PASSWORD': 'pokeapi',
-        'HOST': 'localhost',
-        'PORT': '',
-        'CONN_MAX_AGE': 30
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
     },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'pokeapi',
+    #     'USER': 'root',
+    #     'PASSWORD': 'pokeapi',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    #     'CONN_MAX_AGE': 30
+    # },
 }
 
 CACHES = {

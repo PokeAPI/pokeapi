703ad31d702d01393de1e8aebd50eedf175c9847
Charles Marttinen <mail@charlesmarttinen.ca>
2018-10-29 16:35:51 -0400

    fix: Add fallback value to LanguageEnum

--- graphql_api/language_enum.py
+++ graphql_api/language_enum.py
@@ -1,7 +1,10 @@
 import graphene as g
 from pokemon_v2 import models
 
-LanguageEnum = g.Enum(  # pylint: disable=invalid-name
-    "LanguageEnum",
-    [(l.name.upper().replace("-", "_"), l.name) for l in models.Language.objects.all()],
-)
+_languages = [
+    (l.name.upper().replace("-", "_"), l.name) for l in models.Language.objects.all()
+]
+if len(_languages) == 0:
+    _languages = [("EN", "en")]
+
+LanguageEnum = g.Enum("LanguageEnum", _languages)

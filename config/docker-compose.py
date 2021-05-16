c0dd3004de3d86e6fc8ff9f417eb1fd44af80cb6
Alessandro Pezzè <igougi.ui@gmail.com>
2021-05-16 22:13:07 +0200

    chore: lint

--- config/docker-compose.py
+++ config/docker-compose.py
@@ -9,14 +9,14 @@ DATABASES = {
         "USER": os.environ.get("POSTGRES_USER", "ash"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "pokemon"),
         "HOST": os.environ.get("POSTGRES_HOST", "db"),
-        "PORT": os.environ.get("POSTGRES_PORT", 5432)
+        "PORT": os.environ.get("POSTGRES_PORT", 5432),
     }
 }
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get("REDIS_CONNECTION_STRING", "redis://cache:6379/1") ,
+        "LOCATION": os.environ.get("REDIS_CONNECTION_STRING", "redis://cache:6379/1"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },

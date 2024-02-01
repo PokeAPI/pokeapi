63bf5179a7551100cc3991551f829b1ae53574e4
Naramsim <igougi.ui@gmail.com>
2024-02-01 18:49:24 +0100

    refactor: use compose V2

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    os.environ.get("ADMINS", "Paul Hallett,paulandrewhallett@gmail.com").split(","),
+    os.environ.get("ADMINS", "Pokeapi,team@pokeapi.co").split(","),
 )
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"

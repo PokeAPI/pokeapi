8f9fd71aa65b3b5d331a36489584164140fd8202
Robert Williams <rwilliams651@gmail.com>
2024-02-03 21:56:25 -0600

    removing unnecessary line

--- pokemon_v2/serializers.py
+++ pokemon_v2/serializers.py
@@ -2343,10 +2343,10 @@ class MoveDetailSerializer(serializers.ModelSerializer):
         data = MoveEffectEffectTextSerializer(
             effect_texts, many=True, context=self.context
         ).data
+        
         effect_entries = data[0]
         for i, k in enumerate(effect_entries):
-            if '$effect_chance%' in effect_entries[k]:
-                data[0][k] = effect_entries[k].replace('$effect_chance', f'{obj.move_effect_chance}')
+            data[0][k] = effect_entries[k].replace('$effect_chance', f'{obj.move_effect_chance}')
     
         return data
 

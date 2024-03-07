a5aff843c9cad97620062122d1f673357a00c919
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-03-07 01:27:05 -0600

    docs: overview

--- config/settings.py
+++ config/settings.py
@@ -140,8 +140,23 @@ TEMPLATES = [
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "pokeapi",
-    "DESCRIPTION": "A RESTful API for Pokémon.",
+    "TITLE": "PokéAPI",
+    "DESCRIPTION": """All the Pokémon data you'll ever need in one place, easily accessible through a modern free open-source RESTful API.
+
+## What is this?
+
+This is a full RESTful API linked to an extensive database detailing everything about the Pokémon main game series.
+
+We've covered everything from Pokémon to Berry Flavors.
+
+## Where do I start?
+
+We have awesome [documentation](https://pokeapi.co/docs/v2) on how to use this API. It takes minutes to get started.
+
+This API will always be publicly available and will never require any extensive setup process to consume.
+
+Created by [**Paul Hallett**(]https://github.com/phalt) and other [**PokéAPI contributors***](https://github.com/PokeAPI/pokeapi#contributing) around the world. Pokémon and Pokémon character names are trademarks of Nintendo.
+    """,
     "SERVERS": [{"url": "https://pokeapi.co"}],
     "EXTERNAL_DOCS": {"url": "https://pokeapi.co/docs/v2"},
     "VERSION": "2.7.0",

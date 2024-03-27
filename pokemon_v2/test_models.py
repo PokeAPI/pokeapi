481054981d79943f991f61db7702e829aac3a340
Brennan Gravanda <bgravanda3@gmail.com>
2024-03-27 14:05:40 -0400

    attempted unit test

--- pokemon_v2/test_models.py
+++ pokemon_v2/test_models.py
@@ -9,3 +9,11 @@ class AbilityTestCase(TestCase):
     def fields_are_valid(self):
         smell = Ability.objects.get(name="Smell")
         self.assertEqual(smell.generation_id, 3)
+
+
+class MachineTestCase(TestCase):
+    def setUp(self):
+        MachineVersionLocations.objects.create(machine_id=1,version_group_id=1, location_id=1)
+    def fields_are_valid(self):
+        machine = Machine.objects.get(machine_id=1)
+        self.assertEqual(machine.version_group_id, 1)

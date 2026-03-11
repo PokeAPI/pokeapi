961635a666b7b20e11d2323adf9210c222ad0c25
Naramsim <igougi.ui@gmail.com>
2026-03-11 17:54:04 +0100

    refactor: format

--- pokemon_v2/tests.py
+++ pokemon_v2/tests.py
@@ -5861,7 +5861,7 @@ class APITests(APIData, APITestCase):
     def test_meta_api(self):
         response = self.client.get("{}/meta/".format(API_V2))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(datetime.fromisoformat(response.data['deploy_date']))
-        self.assertEqual(25, len(response.data['deploy_date']))
-        self.assertEqual(40, len(response.data['hash']))
+        self.assertTrue(datetime.fromisoformat(response.data["deploy_date"]))
+        self.assertEqual(25, len(response.data["deploy_date"]))
+        self.assertEqual(40, len(response.data["hash"]))
         self.assertIn("tag", response.data)

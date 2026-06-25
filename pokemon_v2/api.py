ecdfb91b25bb87b4e1f98ae1b8dbbbd494e39588
Julien Marcon <julgamer1@gmail.com>
2026-06-25 22:10:22 +0200

    For pokemon with gender_rate=8 (exclusively female), fill null female sprite keys with their default counterparts across all sprite sections.

--- pokemon_v2/api.py
+++ pokemon_v2/api.py
@@ -754,6 +754,11 @@ class PokemonResource(PokeapiCommonViewset):
     serializer_class = PokemonDetailSerializer
     list_serializer_class = PokemonSummarySerializer
 
+    def get_queryset(self):
+        if self.action == "retrieve":
+            return Pokemon.objects.all().select_related("pokemon_species")
+        return super().get_queryset()
+
 
 @extend_schema(
     description="A Pokémon Species forms the basis for at least one Pokémon. Attributes of a Pokémon species are shared across all varieties of Pokémon within the species. A good example is Wormadam; Wormadam is the species which can be found in three different varieties, Wormadam-Trash, Wormadam-Sandy and Wormadam-Plant.",

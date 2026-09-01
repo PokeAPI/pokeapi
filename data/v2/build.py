f90b9f5ed783f67501da4e4165fca9c962ea20b9
Okapi1023 <lutfunjoya@gmail.com>
2026-09-01 20:47:37 +0100

    Fix syntax error in build.py

--- data/v2/build.py
+++ data/v2/build.py
@@ -1746,9 +1746,10 @@ def _build_pokemons():
                             info,
                             "png",
                         ),
+                    },
                     "champions": {
                         "front_default": try_image_names(poke_sprites + gen_ix + "champions/", info, "png"),
-                    }
+                    },
                 },
             },
         }
